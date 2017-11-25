77197f38cde96dc10861f9dec23892456092ef5a
Himanshu Arora <himanshuarora9419@gmail.com>
2017-11-25 23:58:23 +0530

    controller as syntax we need to add alias with every property in html and we doesn;t need to pas scope to every controller function  it will get from angular as keyword this

--- nested and multiple controller/index.js
+++ nested and multiple controller/index.js
@@ -3,10 +3,11 @@ var app= angular.module("NestedController",[]);
 app.controller("ctrl1",ctrl1);
 app.controller("ctrl2",ctrl2);
 
-function ctrl1($scope){
-    $scope.testProp="ctrl1";
+function ctrl1(){
+    //var this=$scope //angular do something like this
+    this.testProp="ctrl1";
 }
 
-function ctrl2($scope){
-    //$scope.testProp="ctrl2";
+function ctrl2(){
+    this.testProp="ctrl2";
 }
