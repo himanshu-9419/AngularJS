f72f91903adb147de3f08f95ee048c96838aec43
Himanshu Arora <himanshuarora9419@gmail.com>
2017-11-25 23:39:49 +0530

    multiple controller

---
+++ nested and multiple controller/index.js
@@ -0,0 +1,12 @@
+var app= angular.module("NestedController",[]);
+
+app.controller("ctrl1",ctrl1);
+app.controller("ctrl2",ctrl2);
+
+function ctrl1($scope){
+    $scope.testProp="ctrl1";
+}
+
+function ctrl2($scope){
+    $scope.testProp="ctrl2";
+}
