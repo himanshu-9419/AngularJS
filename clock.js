5da6f08a8719fa60b741098c0bd4bdb4b8434977
Himanshu Arora <himanshuarora9419@gmail.com>
2017-11-25 22:56:34 +0530

    2way data binding using ng-model

--- clock.js
+++ clock.js
@@ -4,6 +4,7 @@ module.controller("Clock",Clock);
 function Clock($scope){
     var date = new Date();   
     $scope.time=date.toTimeString();
+    $scope.userName="Himanshu Arora";
     $scope.updateTimePeriod=function(){
         var date = new Date();
         $scope.time = date.toTimeString();
