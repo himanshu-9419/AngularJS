53399fae6e4345655e0c7baf17fd371031bde2fb
Himanshu Arora <himanshuarora9419@gmail.com>
2017-11-27 01:18:10 +0530

    save user using http post

--- ContactApp/user.controller.js
+++ ContactApp/user.controller.js
@@ -4,9 +4,17 @@
     function ngmodelFunc(UserDataSvc) {
         var self=this;
         UserDataSvc.getUsers().then(data => { self.users = data; self.selectedUser = self.users[0];console.log(data);});
-        
+        self.editMode=false;
         self.selectUser = function (index) {
             this.selectedUser = self.users[index];
+        }
+        self.toggleEditMode=function(){
+            self.editMode=!self.editMode;
+        }
+        this.saveUser=function(){
+            self.editMode = !self.editMode;
+            var userData=self.selectedUser;
+            UserDataSvc.saveUser(userData);
         }
     }
 })()
