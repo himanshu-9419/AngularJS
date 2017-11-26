e0b4db8a5a8b2778cc2515556059786dbc7417ae
Himanshu Arora <himanshuarora9419@gmail.com>
2017-11-26 22:45:48 +0530

    factory service

--- ContactApp/app.js
+++ ContactApp/app.js
@@ -15,6 +15,8 @@ app.value("LoggingSvc",function(){console.log("Hello")})
 //     "author": "Himanshu"
 // });
 
+
+//factory service also support dependency injection as funcrion given is clalled by anguar
 app.factory("AppFactorySvc", function (AppNameSvc){
     return AppNameSvc.name;
 })
